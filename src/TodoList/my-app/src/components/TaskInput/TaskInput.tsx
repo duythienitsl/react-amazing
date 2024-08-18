6a28d7d5c37c0f523767d63c2da383ce11ebddd2
Thien Hoang Duy <thien.hoang@DASGNMC13.local>
2024-08-18 17:05:48 +0700

    Todolist

---
+++ src/TodoList/my-app/src/components/TaskInput/TaskInput.tsx
@@ -0,0 +1,50 @@
+import { useState } from 'react';
+import { Todo } from '../../@types/todo.type';
+import styles from './taskInput.module.scss';
+
+interface TaskInputProps {
+  addTodo: (name: string) => void;
+  editTodo: (name: string) => void;
+  finishEditTodo: () => void;
+  currentTodo: Todo | null;
+}
+
+export default function TaskInput(props: TaskInputProps) {
+  const { addTodo, currentTodo, editTodo, finishEditTodo } = props;
+  const [name, setName] = useState<string>('');
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    if (currentTodo) {
+      finishEditTodo();
+      if (name) {
+        setName('');
+      }
+    } else {
+      addTodo(name);
+      setName('');
+    }
+  };
+
+  const onChangeInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target;
+    if (currentTodo) {
+      editTodo(value);
+    } else {
+      setName(value);
+    }
+  };
+  return (
+    <div>
+      <h1 className={styles.title}>Todo list</h1>
+      <form action='' className={styles.form} onSubmit={handleSubmit}>
+        <input
+          type='text'
+          placeholder='caption goes here'
+          value={currentTodo ? currentTodo.name : name}
+          onChange={onChangeInput}
+        />
+        <button type='submit'> {currentTodo ? '✔️' : '➕'}</button>
+      </form>
+    </div>
+  );
+}
---
+++ src/TodoList/my-app/src/components/TaskList/TaskList.tsx
@@ -0,0 +1,46 @@
+import { Todo } from '../../@types/todo.type';
+import styles from './taskList.module.scss';
+
+interface TaskListProps {
+  doneTaskList?: boolean;
+  todos: Todo[];
+  handleDoneTodo: (id: string, done: boolean) => void;
+  startEditTodo: (id: string) => void;
+  deleteTodo: (id: string) => void;
+}
+
+export default function TaskList(props: TaskListProps) {
+  const { doneTaskList, todos, handleDoneTodo, startEditTodo, deleteTodo } = props;
+  const onChangeCheckbox = (idTodo: string) => (event: React.ChangeEvent<HTMLInputElement>) => {
+    handleDoneTodo(idTodo, event.target.checked);
+  };
+
+  return (
+    <div className='mb-2'>
+      <h2 className={styles.title}>{doneTaskList ? 'Hoàn thành' : 'Chưa hoàn thành'}</h2>
+      <div className={styles.tasks}>
+        {todos.map((todo) => (
+          <div className={styles.task} key={todo.id}>
+            <input
+              type='checkbox'
+              checked={todo.done}
+              className={styles.taskCheckbox}
+              onChange={onChangeCheckbox(todo.id)}
+            />
+            <span className={`${styles.taskName} ${todo.done ? styles.taskNameDone : ''}`}>{todo.name}</span>
+            <div className={styles.taskActions}>
+              <button className={styles.taskBtn} onClick={() => startEditTodo(todo.id)}>
+                {' '}
+                🖊️
+              </button>
+              <button className={styles.taskBtn} onClick={() => deleteTodo(todo.id)}>
+                {' '}
+                🗑️
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
